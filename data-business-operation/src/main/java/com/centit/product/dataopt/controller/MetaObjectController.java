c4dc30d4f249d484c0ee221564289618bb902482
杨淮生 <codefan@centit.com>
2019-03-23 10:19:03 +0800

    搭建测试环境成功. yeah

--- data-business-operation/src/main/java/com/centit/product/dataopt/controller/MetaObjectController.java
+++ data-business-operation/src/main/java/com/centit/product/dataopt/controller/MetaObjectController.java
@@ -1,5 +1,23 @@
 package com.centit.product.dataopt.controller;
 
-public class MetaObjectController {
+import com.centit.framework.core.controller.BaseController;
+import com.centit.product.dataopt.service.MetaObjectService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 对数据库进行简单的增删改查，这个接口不能对外公开
+ *
+ * 对外公开的应该式自定义表单对应的额接口
+ */
+@RestController
+@RequestMapping(value = "chart")
+@Api(value = "基于元数据的数据访问服务", tags = "数据访问")
+public class MetaObjectController extends BaseController {
+
+    @Autowired
+    private MetaObjectService metaObjectService;
 
 }

fd8134a813b4b8bd9fc32c1562361036c339ce48
zhf <zhf@centit.com>
2019-08-21 16:58:51 +0800

    元数据查询条件更改

--- database-metadata/src/main/java/com/centit/product/metadata/dao/MetaTableDao.java
+++ database-metadata/src/main/java/com/centit/product/metadata/dao/MetaTableDao.java
@@ -1,18 +1,28 @@
 package com.centit.product.metadata.dao;
 
+import com.centit.framework.core.dao.CodeBook;
 import com.centit.framework.jdbc.dao.BaseDaoImpl;
 import com.centit.product.metadata.po.MetaRelation;
 import com.centit.support.algorithm.CollectionsOpt;
 import com.centit.product.metadata.po.MetaTable;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Repository
 public class MetaTableDao extends BaseDaoImpl<MetaTable, String> {
     @Override
     public Map<String, String> getFilterField() {
-        return null;
+        if( filterField == null) {
+            filterField = new HashMap<>();
+            filterField.put("accessType", CodeBook.IN_HQL_ID);
+            filterField.put("tableType", CodeBook.IN_HQL_ID);
+            filterField.put("databaseCode", CodeBook.EQUAL_HQL_ID);
+            filterField.put("tableId", CodeBook.EQUAL_HQL_ID);
+            filterField.put("tableName", CodeBook.EQUAL_HQL_ID);
+        }
+        return  filterField;
     }
 
     public MetaTable getMetaTable(String databaseCode, String tableName) {

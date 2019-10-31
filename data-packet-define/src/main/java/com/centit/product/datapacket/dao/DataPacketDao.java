dc669b10c147ada6d1285addbd576754ba548b87
zhf <zhf@centit.com>
2019-10-31 13:50:11 +0800

    Revert "增加数据包查询条件"
    This reverts commit b4ff1a3

--- data-packet-define/src/main/java/com/centit/product/datapacket/dao/DataPacketDao.java
+++ data-packet-define/src/main/java/com/centit/product/datapacket/dao/DataPacketDao.java
@@ -1,22 +1,15 @@
 package com.centit.product.datapacket.dao;
 
-import com.centit.framework.core.dao.CodeBook;
 import com.centit.framework.jdbc.dao.BaseDaoImpl;
 import com.centit.product.datapacket.po.DataPacket;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Repository
 public class DataPacketDao extends BaseDaoImpl<DataPacket, String> {
     @Override
     public Map<String, String> getFilterField() {
-        if (filterField == null) {
-            filterField = new HashMap<>();
-            filterField.put("packetName", CodeBook.LIKE_HQL_ID);
-            filterField.put("packetId", CodeBook.EQUAL_HQL_ID);
-        }
-        return filterField;
+        return null;
     }
 }

6f5b61013581d899088dca03d339a3d3d24fb7b9
zhf <zhf@centit.com>
2019-12-04 10:27:06 +0800

    Merge branch 'master' of http://gitlab.centit.com/gitlab/ctm/centit-metadata-commons
    # Conflicts:
    #	business-operation-core/src/main/java/com/centit/product/dataopt/dataset/ExcelDataSet.java

--- business-operation-core/src/main/java/com/centit/product/dataopt/dataset/ExcelDataSet.java
+++ business-operation-core/src/main/java/com/centit/product/dataopt/dataset/ExcelDataSet.java
@@ -2,9 +2,13 @@ package com.centit.product.dataopt.dataset;
 
 import com.centit.product.dataopt.core.DataSet;
 import com.centit.product.dataopt.core.SimpleDataSet;
+import com.centit.support.report.ExcelExportUtil;
 import com.centit.support.report.ExcelImportUtil;
 import com.centit.support.report.ExcelTypeEnum;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ExcelDataSet extends FileDataSet {
@@ -37,6 +41,14 @@ public class ExcelDataSet extends FileDataSet {
 }
     @Override
     public void save(DataSet dataSet) {
-
+        List<Object[]> fields = new ArrayList<>();
+        for(Map<String, Object> map : dataSet.getData()){
+            fields.add(map.keySet().toArray());
+        }
+        try {
+            ExcelExportUtil.appendDataToExcelSheet(this.getFilePath(),0, fields,null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

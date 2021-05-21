532ee8613108ae13d4a1a9b3f1482cfa49549a89
zhf <zhf@centit.com>
2021-05-21 13:59:28 +0800

    使用dde数据库接口

--- database-metadata/src/main/java/com/centit/product/metadata/po/SourceInfo.java
+++ database-metadata/src/main/java/com/centit/product/metadata/po/SourceInfo.java
@@ -2,13 +2,12 @@ package com.centit.product.metadata.po;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
+import com.centit.dde.transaction.vo.ISourceInfo;
 import com.centit.framework.core.dao.DictionaryMap;
-import com.centit.product.metadata.vo.ISourceInfo;
 import com.centit.support.database.orm.GeneratorCondition;
 import com.centit.support.database.orm.GeneratorTime;
 import com.centit.support.database.orm.GeneratorType;
 import com.centit.support.database.orm.ValueGenerator;
-import com.centit.support.database.utils.DataSourceDescription;
 import com.centit.support.security.AESSecurityUtils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -26,60 +25,60 @@ import java.util.Date;
 @Data
 @Entity
 @Table(name = "F_DATABASE_INFO")
-@ApiModel(value="集成资源库信息对象",description="集成资源库信息对象 DatabaseInfo")
-public class SourceInfo implements ISourceInfo,Serializable {
+@ApiModel(value = "集成资源库信息对象", description = "集成资源库信息对象 DatabaseInfo")
+public class SourceInfo implements ISourceInfo, Serializable {
     private static final long serialVersionUID = 1L;
 
     public static final String DESKEY = AESSecurityUtils.AES_DEFAULT_KEY;
 
     @Id
     @Column(name = "DATABASE_CODE")
-    @ApiModelProperty(value = "数据库代码",name = "databaseCode")
+    @ApiModelProperty(value = "数据库代码", name = "databaseCode")
     @ValueGenerator(strategy = GeneratorType.UUID22)
     private String databaseCode;
 
     @Column(name = "OS_ID")
-    @ApiModelProperty(value = "系统代码",name = "osId")
+    @ApiModelProperty(value = "系统代码", name = "osId")
     private String osId;
 
     @Column(name = "DATABASE_NAME")
     @Length(max = 100, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "数据库名",name = "databaseName")
+    @ApiModelProperty(value = "数据库名", name = "databaseName")
     private String databaseName;
 
     @Column(name = "DATABASE_URL")
     @Length(max = 1000, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "数据库地址",name = "databaseUrl")
+    @ApiModelProperty(value = "数据库地址", name = "databaseUrl")
     private String databaseUrl;
 
     @Column(name = "USERNAME")
     @Length(max = 100, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "数据库用户名",name = "username")
+    @ApiModelProperty(value = "数据库用户名", name = "username")
     private String username;
 
     @Column(name = "PASSWORD")
     @Length(max = 100, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "数据库密码",name = "password")
+    @ApiModelProperty(value = "数据库密码", name = "password")
     private String password;
 
     @Column(name = "DATABASE_DESC")
     @Length(max = 500, message = "字段长度不能大于{max}")
-    @ApiModelProperty(value = "数据库描述信息",name = "databaseDesc")
+    @ApiModelProperty(value = "数据库描述信息", name = "databaseDesc")
     private String databaseDesc;
 
-    @ApiModelProperty(value = "修改时间",name = "lastModifyDate")
-    @ValueGenerator( strategy= GeneratorType.FUNCTION, value = "today()", condition = GeneratorCondition.ALWAYS, occasion = GeneratorTime.ALWAYS )
+    @ApiModelProperty(value = "修改时间", name = "lastModifyDate")
+    @ValueGenerator(strategy = GeneratorType.FUNCTION, value = "today()", condition = GeneratorCondition.ALWAYS, occasion = GeneratorTime.ALWAYS)
     @Column(name = "LAST_MODIFY_DATE")
     private Date lastModifyDate;
 
-    @ApiModelProperty(value = "创建人",name = "CREATED")
+    @ApiModelProperty(value = "创建人", name = "CREATED")
     @Column(name = "CREATED")
     @Length(max = 32, message = "字段长度不能大于{max}")
     @DictionaryMap(fieldName = "createUserName", value = "userCode")
     private String created;
 
-    @ApiModelProperty(value = "创建时间",name = "CREATE_TIME")
-    @ValueGenerator( strategy= GeneratorType.FUNCTION, value = "today()")
+    @ApiModelProperty(value = "创建时间", name = "CREATE_TIME")
+    @ValueGenerator(strategy = GeneratorType.FUNCTION, value = "today()")
     @Column(name = "CREATE_TIME")
     private Date createTime;
 
@@ -88,10 +87,11 @@ public class SourceInfo implements ISourceInfo,Serializable {
     @Basic(fetch = FetchType.LAZY)
     private JSONObject extProps;
 
-    @ApiModelProperty(value = "资源类型,D:关系数据库 M:MongoDb R:redis E:elssearch K:kafka B:rabbitmq,H http服务",name = "SOURCE_TYPE")
+    @ApiModelProperty(value = "资源类型,D:关系数据库 M:MongoDb R:redis E:elssearch K:kafka B:rabbitmq,H http服务", name = "SOURCE_TYPE")
     @Column(name = "SOURCE_TYPE")
     private String sourceType;
     // Constructors
+
     /**
      * default constructor
      */
@@ -115,9 +115,9 @@ public class SourceInfo implements ISourceInfo,Serializable {
 
     public void setPassword(String password) {
         if (StringUtils.isNotBlank(password)) {
-            if(  password.startsWith("cipher:")) {
+            if (password.startsWith("cipher:")) {
                 this.password = password;
-            }else {
+            } else {
                 this.password = "cipher:" + AESSecurityUtils.encryptAndBase64(
                     password, SourceInfo.DESKEY);
             }
@@ -126,15 +126,16 @@ public class SourceInfo implements ISourceInfo,Serializable {
 
     @Override
     @JSONField(serialize = false)
-    public String getClearPassword(){
+    public String getClearPassword() {
         return AESSecurityUtils.decryptBase64String(
             getPassword().substring(7), SourceInfo.DESKEY);
     }
 
     @Override
     public boolean equals(Object dbco) {
-        if (this == dbco)
+        if (this == dbco) {
             return true;
+        }
 
         if (dbco instanceof ISourceInfo) {
             ISourceInfo dbc = (ISourceInfo) dbco;

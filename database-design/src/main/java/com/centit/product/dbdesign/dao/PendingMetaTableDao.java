b6a2ef5276fcbbef8bff09274204be386f6509b2
xue_qk <xue_qk@centit.com>
2021-11-26 14:56:45 +0800

    1.修改数据库返回workflowOptType字段为workFlowOptType

--- database-design/src/main/java/com/centit/product/dbdesign/dao/PendingMetaTableDao.java
+++ database-design/src/main/java/com/centit/product/dbdesign/dao/PendingMetaTableDao.java
@@ -53,7 +53,7 @@ public class PendingMetaTableDao extends BaseDaoImpl<PendingMetaTable, String> {
      * @return
      */
     public JSONArray getPendingMetaTableList(Map<String, Object> parameters) {
-        String sql = " SELECT  A.DATABASE_NAME , B.TABLE_ID, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_LABEL_NAME, B.TABLE_COMMENT, B.TABLE_STATE, B.WORKFLOW_OPT_TYPE, B.UPDATE_CHECK_TIMESTAMP, B.LAST_MODIFY_DATE,\n" +
+        String sql = " SELECT  A.DATABASE_NAME , B.TABLE_ID, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_LABEL_NAME, B.TABLE_COMMENT, B.TABLE_STATE, B.WORKFLOW_OPT_TYPE AS WORK_FLOW_OPT_TYPE, B.UPDATE_CHECK_TIMESTAMP, B.LAST_MODIFY_DATE,\n" +
             " B.RECORDER, B.PRIMARY_KEY, B.TABLE_TYPE  FROM F_DATABASE_INFO A JOIN F_PENDING_META_TABLE B ON A.DATABASE_CODE = B.DATABASE_CODE " +
             "  WHERE  1 = 1 [ :topUnit | AND A.TOP_UNIT = :topUnit ]  [ :databaseCode | AND A.DATABASE_CODE = :databaseCode ] " +
             "  [ :(like)tableName | AND B.TABLE_NAME LIKE :tableName ]  [ :(like)tableLabelName | AND B.TABLE_NAME LIKE :tableLabelName ]  ";
@@ -67,7 +67,7 @@ public class PendingMetaTableDao extends BaseDaoImpl<PendingMetaTable, String> {
      * @return
      */
     public JSONArray getPendingMetaTableListByOptId(String optId) {
-        String sql = " SELECT A.ID, B.TABLE_ID, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_LABEL_NAME, B.TABLE_COMMENT, B.TABLE_STATE, B.WORKFLOW_OPT_TYPE, B.UPDATE_CHECK_TIMESTAMP, B.LAST_MODIFY_DATE,\n" +
+        String sql = " SELECT A.ID, B.TABLE_ID, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_LABEL_NAME, B.TABLE_COMMENT, B.TABLE_STATE, B.WORKFLOW_OPT_TYPE AS WORK_FLOW_OPT_TYPE, B.UPDATE_CHECK_TIMESTAMP, B.LAST_MODIFY_DATE,\n" +
             " B.RECORDER, B.PRIMARY_KEY, B.TABLE_TYPE ,C.DATABASE_NAME FROM  F_TABLE_OPT_RELATION A JOIN F_PENDING_META_TABLE B ON  A.TABLE_ID = B.TABLE_ID " +
             "  JOIN  f_database_info C ON B.DATABASE_CODE =C.DATABASE_CODE  WHERE A.OPT_ID = ? ";
         return DatabaseOptUtils.listObjectsBySqlAsJson(this, sql, new Object[]{optId});
--- database-metadata/src/main/java/com/centit/product/metadata/dao/MetaTableDao.java
+++ database-metadata/src/main/java/com/centit/product/metadata/dao/MetaTableDao.java
@@ -37,7 +37,7 @@ public class MetaTableDao extends BaseDaoImpl<MetaTable, String> {
      * @return
      */
     public JSONArray getMetaTableList(Map<String, Object> parameters) {
-        String sql = " SELECT A.DATABASE_NAME , B.TABLE_ID, B.TABLE_LABEL_NAME, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_TYPE, B.ACCESS_TYPE, B.TABLE_COMMENT, B.WORKFLOW_OPT_TYPE, B.RECORD_DATE,\n" +
+        String sql = " SELECT A.DATABASE_NAME , B.TABLE_ID, B.TABLE_LABEL_NAME, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_TYPE, B.ACCESS_TYPE, B.TABLE_COMMENT, B.WORKFLOW_OPT_TYPE AS WORK_FLOW_OPT_TYPE, B.RECORD_DATE,\n" +
             " B.RECORDER, B.UPDATE_CHECK_TIMESTAMP, B.FULLTEXT_SEARCH, B.WRITE_OPT_LOG, B.OBJECT_TITLE   FROM F_DATABASE_INFO A JOIN F_MD_TABLE B ON A.DATABASE_CODE = B.DATABASE_CODE " +
             "  WHERE  1 = 1 [ :topUnit | AND A.TOP_UNIT = :topUnit ]  [ :databaseCode | AND A.DATABASE_CODE = :databaseCode ] " +
             " [ :(like)tableName | AND B.TABLE_NAME LIKE :tableName ]  [ :(like)tableLabelName | AND B.TABLE_NAME LIKE :tableLabelName ] ";
@@ -50,7 +50,7 @@ public class MetaTableDao extends BaseDaoImpl<MetaTable, String> {
      * @return
      */
     public JSONArray getMetaTableListByOptId(String optId) {
-        String sql = "SELECT A.ID, B.TABLE_ID, B.TABLE_LABEL_NAME, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_TYPE, B.ACCESS_TYPE, B.TABLE_COMMENT, B.WORKFLOW_OPT_TYPE, B.RECORD_DATE,\n" +
+        String sql = "SELECT A.ID, B.TABLE_ID, B.TABLE_LABEL_NAME, B.DATABASE_CODE, B.TABLE_NAME, B.TABLE_TYPE, B.ACCESS_TYPE, B.TABLE_COMMENT, B.WORKFLOW_OPT_TYPE AS WORK_FLOW_OPT_TYPE, B.RECORD_DATE,\n" +
             " B.RECORDER, B.UPDATE_CHECK_TIMESTAMP, B.FULLTEXT_SEARCH, B.WRITE_OPT_LOG, B.OBJECT_TITLE ,C.DATABASE_NAME FROM  F_TABLE_OPT_RELATION A JOIN f_md_table B ON  A.TABLE_ID = B.TABLE_ID " +
             "  JOIN  f_database_info C ON B.DATABASE_CODE =C.DATABASE_CODE  WHERE A.OPT_ID = ? ";
         return DatabaseOptUtils.listObjectsBySqlAsJson(this, sql, new Object[]{optId});

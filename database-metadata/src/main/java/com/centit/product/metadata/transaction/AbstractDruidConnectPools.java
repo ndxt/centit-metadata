a0ffe3e374c167b01004c7875e0a2f8b298c1895
周抄 <17625999642@163.com>
2022-05-31 16:07:20 +0800

    修改默认参数

--- database-metadata/src/main/java/com/centit/product/metadata/transaction/AbstractDruidConnectPools.java
+++ database-metadata/src/main/java/com/centit/product/metadata/transaction/AbstractDruidConnectPools.java
@@ -32,7 +32,7 @@ public abstract class AbstractDruidConnectPools {
     private static DruidDataSource mapDataSource(ISourceInfo dsDesc) {
         DruidDataSource ds = new DruidDataSource();
         //失败时是否进行重试连接    true:不进行重试   false：进行重试    设置为false时达蒙数据库会出现问题（会导致达蒙连接撑爆挂掉）
-        ds.setBreakAfterAcquireFailure(BooleanBaseOpt.castObjectToBoolean(dsDesc.getExtProp("breakAfterAcquireFailure"),false));
+        ds.setBreakAfterAcquireFailure(BooleanBaseOpt.castObjectToBoolean(dsDesc.getExtProp("breakAfterAcquireFailure"),true));
         ds.setTimeBetweenConnectErrorMillis(NumberBaseOpt.castObjectToInteger(
             dsDesc.getExtProp("timeBetweenConnectErrorMillis"), 6000));
         ds.setConnectionErrorRetryAttempts(NumberBaseOpt.castObjectToInteger(

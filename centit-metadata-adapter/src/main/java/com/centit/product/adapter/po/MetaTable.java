6a08eaf8c0f49d5112afd4cae29116900a8c6d1b
zhf <zhf@centit.com>
2022-06-01 17:45:32 +0800

    修改bug

--- centit-metadata-adapter/src/main/java/com/centit/product/adapter/po/MetaTable.java
+++ centit-metadata-adapter/src/main/java/com/centit/product/adapter/po/MetaTable.java
@@ -319,7 +319,7 @@ public class MetaTable implements TableInfo, java.io.Serializable {
         }
 
         for (MetaColumn c : mdColumns) {
-            if (c.getPropertyName().equalsIgnoreCase(name)) {
+            if (c.getColumnName().equalsIgnoreCase(name)) {
                 return c;
             }
         }

85a82fbeba77d5e64f8dd656d1fb166279ec6946
codefan <codefan@centit.com>
2023-01-17 17:32:01 +0800

    更改bug保存和数据字典的一致性

--- centit-metadata-adapter/src/main/java/com/centit/product/adapter/api/ISourceInfo.java
+++ centit-metadata-adapter/src/main/java/com/centit/product/adapter/api/ISourceInfo.java
@@ -7,13 +7,16 @@ import com.centit.support.database.metadata.IDatabaseInfo;
  */
 public interface ISourceInfo extends IDatabaseInfo {
     String DATABASE="D";
-    String MONGO_DB="G";
+    String MONGO_DB="M";
     String REDIS="R";
     String ES="E";
     String KAFKA="K";
     String RABBIT_MQ="B";
     String HTTP="H";
-    String EMAIL="M";
+    /**
+     * 邮件服务
+     */
+    String EMAIL="L";
 
     String getSourceType();
 }

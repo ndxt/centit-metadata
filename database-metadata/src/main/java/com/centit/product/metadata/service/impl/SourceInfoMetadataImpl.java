0d337333969ed41cdff2749a1551016f0db4bb53
ndxt_yhs <codefan@centit.com>
2024-06-27 13:05:58 +0800

    给数据库资源获取添加一个缓存

--- database-metadata/src/main/java/com/centit/product/metadata/service/impl/SourceInfoMetadataImpl.java
+++ database-metadata/src/main/java/com/centit/product/metadata/service/impl/SourceInfoMetadataImpl.java
@@ -4,6 +4,8 @@ import com.centit.product.metadata.dao.SourceInfoDao;
 import com.centit.product.metadata.po.SourceInfo;
 import com.centit.product.metadata.service.SourceInfoMetadata;
 import com.centit.product.metadata.service.PropertyConvertor;
+import com.centit.support.common.CachedMap;
+import com.centit.support.common.CachedObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,19 +19,26 @@ public class SourceInfoMetadataImpl implements SourceInfoMetadata {
 
     private PropertyConvertor propertyConvertor;
 
+    private final CachedMap<String, SourceInfo> sourceInfoCache;
+
     public SourceInfoMetadataImpl(){
         this.propertyConvertor = null;
+        this.sourceInfoCache = new CachedMap<>(this::loadSourceInfo, CachedObject.DEFAULT_REFRESH_PERIOD);
     }
 
     public void setPropertyConvertor(PropertyConvertor propertyConvertor) {
         this.propertyConvertor = propertyConvertor;
     }
 
-    @Override
-    public SourceInfo fetchSourceInfo(String databaseCode) {
+    protected SourceInfo loadSourceInfo(String databaseCode) {
         SourceInfo sourceInfo = sourceInfoDao.getDatabaseInfoById(databaseCode);
         if(sourceInfo==null || this.propertyConvertor ==null)
             return sourceInfo;
         return this.propertyConvertor.convertSourceInfo(sourceInfo);
+    }
+
+    @Override
+    public SourceInfo fetchSourceInfo(String databaseCode) {
+        return sourceInfoCache.getCachedValue(databaseCode);
     }
 }

54b9a3c01c7df6e857b9e4a9c67397fc025ffadd
codefan <codefan@centit.com>
2025-03-26 15:34:15 +0800

    添加方法

--- database-metadata/src/test/java/com/centit/test/TestSoap.java
+++ database-metadata/src/test/java/com/centit/test/TestSoap.java
@@ -6,7 +6,6 @@ import com.centit.support.network.HttpExecutorContext;
 import org.dom4j.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
